76bb57defea5fceba56efb176bc7dd5a7134265b
Harsha <pawarharsha474@gmail.com>
2025-07-08 13:22:30 +0000

    Auto-commit: Tue Jul  8 01:22:30 PM UTC 2025

--- SDK/ruby/s3.rb
+++ SDK/ruby/s3.rb
@@ -1,6 +1,11 @@
-require 'aws-sdk-s3'
-require 'pry'
-require 'securerandom'
+# Required libraries
+require 'aws-sdk-s3'  # AWS SDK for S3 operations
+require 'pry'        # Interactive shell for debugging
+require 'securerandom' # For generating secure random strings (UUIDs)
 
-bucket_name =ENV['BUCKET_NAME']
-puts bucket_name
+# S3 Bucket configuration
+bucket_name = ENV['BUCKET_NAME']  # Fetching bucket name from environment variables
+region = 'ca-central-1'           # AWS region where the bucket will be created
+
+
+
