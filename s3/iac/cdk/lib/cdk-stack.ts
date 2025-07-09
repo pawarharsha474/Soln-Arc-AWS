435a37ad64a116f121bbf2fe3a7373538eb2ae87
Harsha <pawarharsha474@gmail.com>
2025-07-09 13:03:18 +0000

    Auto-commit: Wed Jul  9 01:03:18 PM UTC 2025

--- s3/iac/cdk/lib/cdk-stack.ts
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -1,5 +1,5 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
@@ -7,6 +7,8 @@ import { Construct } from 'constructs';
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    s3.Bucket.(this,)
 
     const queue = new sqs.Queue(this, 'CdkQueue', {
       visibilityTimeout: Duration.seconds(300)
