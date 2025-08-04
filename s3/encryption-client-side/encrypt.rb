75933ea5a3038aff5308caa67dc37090e8bbdb05
Harsha <pawarharsha474@gmail.com>
2025-08-04 13:55:36 +0000

    Auto-commit: Mon Aug  4 01:55:36 PM UTC 2025

--- s3/encryption-client-side/encrypt.rb
+++ s3/encryption-client-side/encrypt.rb
@@ -3,17 +3,17 @@ require 'aws-sdk-s3'
 key = OpenSSL::PKey::RSA.new(1024)
 
 bucket='encr-cl-buc1'
-key='hello.txt'
+object_key='hello.txt'
 
 # encryption client
-s3 = Aws::S3::EncryptionV2::Client.new(encryption_key: key)
+s3 = Aws::S3::EncryptionV2::Client.new(encryption_key: object_key)
 
 # round-trip an object, encrypted/decrypted locally
-s3.put_object(bucket:bucket, key:'hello.txt', body:'handshake')
-s3.get_object(bucket:bucket, key:'hello.txt').body.read
+s3.put_object(bucket:bucket, key:object_key, body:'handshake')
+s3.get_object(bucket:bucket, key:object_key).body.read
 #=> 'handshake'
 
 # reading encrypted object without the encryption client
 # results in the getting the cipher text
-Aws::S3::Client.new.get_object(bucket:bucket, key:'hello.txt').body.read
+Aws::S3::Client.new.get_object(bucket:bucket, key:object_key).body.read
 #=> "... cipher text ..."
