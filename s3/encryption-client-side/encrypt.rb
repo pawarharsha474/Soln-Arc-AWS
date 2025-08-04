9846eebfbdd55a28bd28dde5fa5d5cc3d227dbbc
Harsha <pawarharsha474@gmail.com>
2025-08-04 13:40:33 +0000

    Auto-commit: Mon Aug  4 01:40:33 PM UTC 2025

---
+++ s3/encryption-client-side/encrypt.rb
@@ -0,0 +1,18 @@
+require 'openssl'
+key = OpenSSL::PKey::RSA.new(1024)
+
+bucket='encr-cl-buc1'
+key='hello.txt'
+
+# encryption client
+s3 = Aws::S3::Encryption::Client.new(encryption_key: key)
+
+# round-trip an object, encrypted/decrypted locally
+s3.put_object(bucket:bucket, key:'hello.txt', body:'handshake')
+s3.get_object(bucket:bucket, key:'hello.txt').body.read
+#=> 'handshake'
+
+# reading encrypted object without the encryption client
+# results in the getting the cipher text
+Aws::S3::Client.new.get_object(bucket:bucket, key:'hello.txt').body.read
+#=> "... cipher text ..."
